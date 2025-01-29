5f2f6e27a1c53c16cf878e81ffc9678137fae901
vratislavino <vratislavino@gmail.com>
2025-01-29 13:16:58 +0100

    Update Extensions.cs

--- Assets/RPS/Scripts/StateMachine/Extensions.cs
+++ Assets/RPS/Scripts/StateMachine/Extensions.cs
@@ -4,7 +4,17 @@ public static class Extensions
 {
     public static bool? WouldWin(this Symbol a, Symbol b)
     {
-        // Proè?
+        if(a == b) return null;
+
+        if(a == Symbol.Rock)
+            return b == Symbol.Scissors;
+
+        if (a == Symbol.Paper)
+            return b == Symbol.Rock;
+
+        if (a == Symbol.Scissors)
+            return b == Symbol.Paper;
+
         return false;
     }
 }
